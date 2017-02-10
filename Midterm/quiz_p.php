5f4c7ceff23d7c610518ca61e00152cc1dd0d41a
Michael Wolfe <michaelwolfe@zipcoders-MacBook-Pro-6.local>
2017-02-10 15:31:09 -0500

    fixed brackets and indenting

--- Midterm/quiz_p.php
+++ Midterm/quiz_p.php
@@ -6,20 +6,18 @@ echo '<h1>Midterm</h1>';
 require ('../../midterm_spring_2016.php'); // Connect to the db.
 
 // Do query to get questions for a quiz
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	//print_r($_POST);
 
 $answers = "SELECT question_id, answer FROM questions
            JOIN answers USING (question_id)
 		   WHERE quiz_id = 1 AND correct_ans_id = answer_id";
 
-$q_answers = @mysqli_query ($dbc, $answers);  // Run the query.	
+$q_answers = @mysqli_query ($dbc, $answers);  // Run the query.
 
 $answer_count = 0;
 $num = mysqli_num_rows($q_answers);
-	while ($row = mysqli_fetch_array($q_answers, MYSQLI_ASSOC)) 
-	{
+	while ($row = mysqli_fetch_array($q_answers, MYSQLI_ASSOC)){
 		//echo "<p>". $row["question_id"] . "  " . $row["answer"]. "</p>";
 		$id = $row['question_id'];
 		if (isset($_POST[$id]) && ($_POST[$id] == $row['answer']))
@@ -31,82 +29,66 @@ $num = mysqli_num_rows($q_answers);
 		{
 			echo $id . " Try again, dummy! <br />";
 		}
-	} 
+	}
 
 $score = ($answer_count / $num) * 100;
 echo "<br />You answered " . $answer_count . " out of &nbsp" . $num . " questions correct for a score of " . $score . "%.<br />";
-	
-	if ($score <= 70) 
-	{
+
+	if ($score <= 70){
 		echo "Online learning is not for you. Please sign up for a traditional course.";
 	}
-	elseif ($score <= 75)
-	{
+	elseif ($score <= 75){
 		echo "You may be fine, but it's best if you stick with traditional courses for now.";
 	}
-	elseif ($score <= 85)
-	{
+	elseif ($score <= 85){
 		echo "You seem fairly ready for online learning.";
 	}
-	elseif ($score <= 95)
-	{
+	elseif ($score <= 95){
 		echo "You seem ready for online learning.";
 	}
-	elseif ($score <= 100)
-	{
+	elseif ($score <= 100){
 		echo "You are ready for online learning. Sign up!";
 	}
-}
-
-    
-
-else
-{
+} else {
 	$q = "SELECT quiz_id, question, question_id, correct_ans_id
 		 FROM questions WHERE quiz_id = 1"; // ORDER BY RAND()";
 	$r = @mysqli_query ($dbc, $q);  // Run the query.
 
-		echo '<form action="quiz_p.php" method="post"><table cellspacing="3" cellpadding="3" width="75%">
-	';
+		echo '<form action="quiz_p.php" method="post"><table cellspacing="3" cellpadding="3" width="75%">';
 
 	// Display question
 
 	// variable declaration
 	$count = 1;
-	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) 
-	{ 
+	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)){
 		echo "<tr><td>$count. " . $row['question'] . '</td></tr>';
 		$count++;
-		
+
 		$a = "SELECT answer, answer_id FROM answers WHERE question_id = ". $row['question_id'];
 		$result = @mysqli_query ($dbc, $a);  // Run the query.
-		
-			while ($answer_row = mysqli_fetch_array($result, MYSQLI_ASSOC)) 
-			{ 
+
+			while ($answer_row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
 			echo "<tr><td>";
 			create_radio($row['question_id'], $answer_row['answer']);
 			echo '</td></tr>';
 			}
-	}	
+	}
 
 	echo '</table><input type="Submit" name="Submit" value="Submit"></form>';
 
 }
 
-function create_radio($name, $value) 
-{
+function create_radio($name, $value){
 
 	// Start the element:
 	echo '<input type="radio" name='. $name. ' value="'. $value . '"';
-	
+
 	// Check for stickiness:
-	if (isset($_POST[$name]) && ($_POST[$name] == $value)) 
-	{
+	if (isset($_POST[$name]) && ($_POST[$name] == $value)){
 		echo ' checked="checked"';
 	}
-	
+
 	// Complete the element:
 	echo " /> $value ";
- 
- } 
+} 
 ?>
--- Midterm/view_quizzes.php
+++ Midterm/view_quizzes.php
@@ -1,4 +1,4 @@
-<?php # - index.php 
+<?php # - index.php
 // Michael Wolfe
 //Midterm
 //03.15.2016
@@ -9,14 +9,12 @@ require ('../../midterm_spring_2016.php'); // Connect to the db.
 // Create the quizzes query
 $select = "SELECT name, not_ready, fairly_ready, seem_ready, ready FROM quizzes
 ORDER BY quiz_id";
-$r = @mysqli_query ($dbc, $select); // Run the query.	
+$r = @mysqli_query ($dbc, $select); // Run the query.
 // Count the number of returned rows:
 $num = mysqli_num_rows($r);
 
-if ($num > 0)
-{ //if it ran OK, display the records
-	if ($r) 
-	{ // If it ran OK, display the records.
+if ($num > 0){ //if it ran OK, display the records
+	if ($r){ // If it ran OK, display the records.
 
 		// Table header.
 		echo '<table align="center" cellspacing="3" cellpadding="3" width="75%">
@@ -26,10 +24,9 @@ if ($num > 0)
 			<td align="left"><b>Seem Ready</b></td>
 			<td align="left"><b>Ready</b></td>
 			</tr>';
-			
+
 		//Fetch and print all the records:
-		while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) 
-		{
+		while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)){
 			echo '<tr>
 			<td align="left">' . $row['name'] . '</td>
 			<td align="left">' . $row['not_ready'] . '</td>
@@ -39,20 +36,20 @@ if ($num > 0)
 			</tr>';
 		}
 	echo '</table>'; // Close the table.
-	
-	mysqli_free_result ($r); // Free up the resources.	
+
+	mysqli_free_result ($r); // Free up the resources.
 	} else { //IF no records returned
-		echo '<p class="error">There are currently no completed quizzes.</p>';	
+		echo '<p class="error">There are currently no completed quizzes.</p>';
 	}
 
 } else { // If it did not run OK.
 
 	// Public message:
 	echo '<p class="error">The current quiz results could not be retrieved. We apologize for any inconvenience.</p>';
-	
+
 	// Debugging message:
 	echo '<p>' . mysqli_error($dbc) . '<br /><br />Query: ' . $q . '</p>';
-	
+
 } // End of if ($r) IF.
 
 mysqli_close($dbc); // Close the database connection.
